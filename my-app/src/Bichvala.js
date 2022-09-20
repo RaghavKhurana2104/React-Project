8b3f80bdaff6c08b11bdee7a5447cf46d47dd297
RaghavKhurana2104 <raghavkhurana60@gmail.com>
2022-09-20 19:57:57 +0530

    second Comment

--- my-app/src/Bichvala.js
+++ my-app/src/Bichvala.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import headphone from './Photos/headphone.png'
-import home from './Photos/home.png'
+
 import fashion from './Photos/fashion.png'
 import toys from './Photos/toys.png'
 import grocery from './Photos/grocery.png'
@@ -12,9 +10,9 @@ import './Middle.css'
 function Bichvala() {
   return (
    <div>
-  <nav class="navbar navbar-expand-sm navbar-dark bg-secondary  justify-content-center" >
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark  justify-content-center" >
     
-    <button className='btn1'><img src={mobile} alt="" className='hdr2' /><br />Smartphones </button>
+    <button className='btn1' ><img src={mobile} alt="" className='hdr2' /><br />Smartphones </button>
      <button className='btn1'><img src={headphone} alt=""  className='hdr2'/><br />Electronics</button>
      <button className='btn1'><img src={fashion} alt=""  className='hdr2'/><br />Fashion</button>
      <button className='btn1'><img src={toys} alt=""  className='hdr2'/><br />Toys</button>
@@ -24,6 +22,13 @@ function Bichvala() {
     
   </nav>
   
+  <div className='container bg-light' style={{height:'20em',marginBlock:'5% ',borderRadius:'1em'}}></div>
+  <div className='container bg-light' style={{height:'20em',marginBlock:'5%',borderRadius:'1em'}}></div>
+  <div className='container bg-light' style={{height:'20em',marginBlock:'5%',borderRadius:'1em'}}></div>
+  <div className='container bg-light' style={{height:'20em',marginBlock:'5%',borderRadius:'1em'}}></div>
+  <div className='container bg-light' style={{height:'20em',marginBlock:'5%',borderRadius:'1em'}}></div>
+  <div className='container bg-light' style={{height:'20em',marginBlock:'5%',borderRadius:'1em'}}></div>
+  <div className='container bg-light' style={{height:'20em',marginBlock:'5%',borderRadius:'1em'}}></div>
   </div> 
   )
 }
